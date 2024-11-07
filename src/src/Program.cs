fd8af6135ece883f0d741a90f163aa49e6977904
mrbuzb <mrbpubg14@gmail.com>
2024-11-06 23:34:12 -0700

    Homework Added

--- src/src/Program.cs
+++ src/src/Program.cs
@@ -1,10 +1,553 @@
-﻿namespace src
+﻿namespace Homework
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Console.Write("4 xonali Son kiriting :");
+            var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var ones = num % 10;
+            var tens = num / 10 % 10;
+            var hundreds = num / 100 % 10;
+            var sousands = num / 1000 % 10;
+            Console.WriteLine($"{sousands}{hundreds}{ones}{tens}");*/
+
+            /*Console.Write("3 xonali Son kiriting :");
+            var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var ones = num % 10;
+            var tens = num / 10 % 10;
+            var hundreds = num / 100 % 10;
+            Console.WriteLine($"{tens}{hundreds}{ones}");*/
+
+            /*Console.Write("3 xonali Son kiriting :");
+            var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var ones = num % 10;
+            var tens = num / 10 % 10;
+            var hundreds = num / 100 % 10;
+            var sousands = num / 1000 % 10;
+            Console.WriteLine($"{tens}{hundreds}{sousands}{ones}");*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num * 1024 + " mb");*/
+
+            /*var num = 0l;
+            num = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(num * 1024 * 1024 * 1024 * 8 + " bit");*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num / 1024 + " tb");*/
+
+            /*var memoryCard = 0;
+            memoryCard = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"2 gb lik dan {memoryCard / 2} ta");*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var secondDegree = num * num;
+            var fourthDegree = secondDegree * secondDegree;
+            var eighthDegree = fourthDegree * fourthDegree;
+            Console.WriteLine(eighthDegree);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var isCouple = false;
+            if (num % 2 == 0)
+            {
+                isCouple = true;
+            }
+            Console.WriteLine(isCouple);*/
+
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var isCouple = false;
+            if (num % 2 != 0)
+            {
+                isCouple = true;
+            }
+            Console.WriteLine(isCouple);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var roomIs2 = false;
+            if (num >= 10 && num <= 99)
+            {
+                roomIs2 = true;
+            }
+            Console.WriteLine(roomIs2);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var roomIs4 = false;
+            if (num >= 1000 && num <= 9999)
+            {
+                roomIs4 = true;
+            }
+            Console.WriteLine(roomIs4);*/
+
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var isDegreeAnd2Rooms = false;
+
+            if (num % 2 == 0 && num >= 10 && num <= 99)
+            {
+                isDegreeAnd2Rooms = true;
+            }
+            Console.WriteLine(isDegreeAnd2Rooms);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var isNotDegreeAnd3Rooms = false;
+
+            if (num % 2 != 0 && num >= 100 && num <= 999)
+            {
+                isNotDegreeAnd3Rooms = true;
+            }
+            Console.WriteLine(isNotDegreeAnd3Rooms);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var istDegreeAnd4Rooms = false;
+
+            if (num % 2 == 0 && num >= 1000 && num <= 9999)
+            {
+                istDegreeAnd4Rooms = true;
+            }
+            Console.WriteLine(istDegreeAnd4Rooms);*/
+
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            var twoOdds = false;
+            if (num1 % 2 != 0 && num2 % 2 != 0)
+            {
+                twoOdds = true;
+            }
+            Console.WriteLine(twoOdds);*/
+
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            var twodegrees = false;
+            if (num1 % 2 == 0 && num2 % 2 == 0)
+            {
+                twodegrees = true;
+            }
+            Console.WriteLine(twodegrees);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            var twoOddAnd2room = false;
+            if (num1 % 2 != 0 && num2 % 2 != 0 && num1 >= 10 && num1 <= 99 &&
+                num2 >= 10 && num2 <= 99)
+            {
+                twoOddAnd2room = true;
+            }
+            Console.WriteLine(twoOddAnd2room);*/
+
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var ones = num % 10;
+            var tenth=num /10% 10;
+            var twoRoomsAreEqual=false;
+            if(ones == tenth)
+            {
+                twoRoomsAreEqual = true;
+            }
+            Console.WriteLine(twoRoomsAreEqual);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var ones = num % 10;
+            var tenth = num / 10 % 10;
+            var greaterThenTen = false;
+            if (ones + tenth>10)
+            {
+                greaterThenTen = true;
+            }
+            Console.WriteLine(greaterThenTen);*/
+
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var ones = num % 10;
+            var tenth = num / 10 % 10;
+            var greaterThenTwenty = false;
+            if (ones * tenth > 10)
+            {
+                greaterThenTwenty = true;
+            }
+            Console.WriteLine(greaterThenTwenty);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var ones = num % 10;
+            var tenth = num / 10 % 10;
+            var greaterThenTwenty = false;
+            if (ones * tenth > 20)
+            {
+                greaterThenTwenty = true;
+            }
+            Console.WriteLine(greaterThenTwenty);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var ones = num % 10;
+            var tenth = num / 10 % 10;
+            var twoRoomsAreOdd = false;
+            if (ones%2!=0&&tenth%2!=0)
+            {
+                twoRoomsAreOdd = true;
+            }
+            Console.WriteLine(twoRoomsAreOdd);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var ones = num % 10;
+            var tenth = num / 10 % 10;
+            var greaterThenFive = false;
+            if (ones > 5 && tenth > 5)
+            {
+                greaterThenFive = true;
+            }
+            Console.WriteLine(greaterThenFive);*/
+
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var dividedIntoThree = false;
+            if (num % 3 == 0)
+            {
+                dividedIntoThree = true;
+            }
+            Console.WriteLine(dividedIntoThree);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var dividedIntoSeven = false;
+            if (num % 7 == 0)
+            {
+                dividedIntoSeven = true;
+            }
+            Console.WriteLine(dividedIntoSeven);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var dividedEightAndThree = false;
+            if (num % 3 == 0 && num % 8 == 0)
+            {
+                dividedEightAndThree = true;
+            }
+            Console.WriteLine(dividedEightAndThree);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var dividedSevenOrNine = false;
+            if (num % 7 == 0 || num % 9 == 0)
+            {
+                dividedSevenOrNine = true;
+            }
+            Console.WriteLine(dividedSevenOrNine);*/
+
+            /*var num = 0;
+            num=Convert.ToInt32(Console.ReadLine());
+            var dividedThree=false;
+            if(num % 3 == 0)
+            {
+                dividedThree = true;
+            }
+            Console.WriteLine(dividedThree);*/
+
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var notDividedFive = false;
+            if (num % 5 != 0)
+            {
+                notDividedFive = true;
+            }
+            Console.WriteLine(notDividedFive);*/
+
+            /*var num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            var notDividedThree = false;
+            if (num % 4 == 0 && num % 3 != 0)
+            {
+                notDividedThree = true;
+            }
+            Console.WriteLine(notDividedThree);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            var firstGreaterSecond = false;
+            if (num1 > num2)
+            {
+                firstGreaterSecond = true;
+            }
+            Console.WriteLine(firstGreaterSecond);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            var firstEqualSecond = false;
+            if (num1 == num2)
+            {
+                firstEqualSecond = true;
+            }
+            Console.WriteLine(firstEqualSecond);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            var num3 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+            var firstGreater = false;
+            if (num1 > num2 && num1 > num3)
+            {
+                firstGreater = true;
+            }
+            Console.WriteLine(firstGreater);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            var num3 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+            var thirdGreator = false;
+            if (num3 > num2 && num3 > num1)
+            {
+                thirdGreator = true;
+            }
+            Console.WriteLine(thirdGreator);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            var num3 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+            var isSecondLittle = false;
+            if (num3 > num2 && num1 > num2)
+            {
+                isSecondLittle = true;
+            }
+            Console.WriteLine(isSecondLittle);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            var twoXGreator = false;
+            if (num1 > num2 * 2)
+            {
+                twoXGreator = true;
+            }
+            Console.WriteLine(twoXGreator);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            var fiveXGreator = false;
+            if (num1 > num2 * 5||num2>num1*5)
+            {
+                fiveXGreator = true;
+            }
+            Console.WriteLine(fiveXGreator);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            var fourXGreator = false;
+            if (num1 > num2 * 4 || num2 > num1 * 4)
+            {
+                fourXGreator = true;
+            }
+            Console.WriteLine(fourXGreator);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 =Convert.ToInt32(Console.ReadLine());
+            num2 =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(TwoBiggest(num1,num2));*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(TwoLittle(num1, num2));*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (TwoRooms(num1, num2))
+            {
+                Console.WriteLine(num1 + num2);
+                Main(args);
+            }
+            Console.WriteLine(num1 * num2);*/
+
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 % 2 == 0 && num2 % 2 == 0)
+            {
+                Console.WriteLine(num1 + num2);
+                Main(args);
+            }
+            Console.WriteLine(num1 * num2);*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 % 2 != 0 && num2 % 2 != 0)
+            {
+                Console.WriteLine(num1 * num2);
+                Main(args);
+            }
+            Console.WriteLine($"{num1} , {num2}");*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            var num3 = 0;
+            var sum = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 % 2 != 0)
+            {
+                sum++;
+            }
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num2 % 2 != 0)
+            {
+                sum++;
+            }
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if (num3 % 2 != 0)
+            {
+                sum++;
+            }
+            Console.WriteLine($"{sum} ta toq");*/
+
+
+            /*var num = 0;
+            num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(SumOfDegrees(num));*/
+
+            /*var num1 = 0;
+            var num2 = 0;
+            var num3 = 0;
+            var num4 = 0;
+            var num5 = 0;
+            var sum = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 % 2 == 0)
+            {
+                sum++;
+            }
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num2 % 2 == 0)
+            {
+                sum++;
+            }
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if (num3 % 2 == 0)
+            {
+                sum++;
+            }
+            num4 = Convert.ToInt32(Console.ReadLine());
+            if (num4 % 2 == 0)
+            {
+                sum++;
+            }
+            num5 = Convert.ToInt32(Console.ReadLine());
+            if (num5 % 2 == 0)
+            {
+                sum++;
+            }
+            Console.WriteLine($"{sum} ta juft");*/
+
+
+
+
+
+
+
+
+            Main(args);
         }
+
+        static int SumOfDegrees(int num)
+        {
+            var sum = 0;
+            var ones = 0;
+            while (true)
+            {
+                ones = num % 10;
+                if (ones % 2 == 0)
+                {
+                    sum++;
+                }
+                num /= 10;
+                if (num == 0)
+                {
+                    break;
+                }
+            }
+            return sum;
+        }
+
+        static int TwoBiggest(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            return b;
+        }
+        static int TwoLittle(int a, int b)
+        {
+            if (a > b)
+            {
+                return b;
+            }
+            return a;
+        }
+
+        static bool TwoRooms(int a, int b)
+        {
+            if (a >= 10 && a <= 99 && a >= 10 && a <= 99)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
     }
 }
